a52d3afaebe1a6c4865190d84d96be84df6f4e24
Xiaozhong Deng <xide@consignor.com>
2018-12-17 13:00:36 +0800

    20181217 2nd

--- Chp05/reverseCiper.py
+++ Chp05/reverseCiper.py
@@ -7,5 +7,6 @@ message_dec = ".daed era meht fo owt fi ,terces a peek nac eerhT"
 i = len(message_dec) - 1
 while i >= 0:
     translated = translated + message_dec[i]
+    print(i, message[i], translated)
     i = i - 1
 print(translated)
