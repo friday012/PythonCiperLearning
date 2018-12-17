63808cace60aecb4cf108312fa63d69fb4b04d98
Xiaozhong Deng <xide@consignor.com>
2018-12-17 12:51:32 +0800

    20181217 begining.

---
+++ Chp05/reverseCiper.py
@@ -0,0 +1,11 @@
+message = 'Three can keep a secret, if two of them are dead.'
+translated = ''
+
+message_dec = ".daed era meht fo owt fi ,terces a peek nac eerhT"
+
+
+i = len(message_dec) - 1
+while i >= 0:
+    translated = translated + message_dec[i]
+    i = i - 1
+print(translated)
